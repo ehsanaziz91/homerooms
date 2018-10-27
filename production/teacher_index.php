ef94b88cc852e4b1bffe645ff8f4077b59cbdee9
ehsanaziz91 <ehsanaziz96@gmail.com>
2018-10-27 17:08:38 +0800

    Update production/teacher_index.php

--- production/teacher_index.php
+++ production/teacher_index.php
@@ -6,6 +6,7 @@
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta http-equiv="REFRESH" content="10; url=http://localhost/homerooms/production/teacher_index.php"> <!--loading page in every 10 second-->
 	<!--<link rel="icon" href="images/favicon.ico" type="image/ico" />-->
 
     <title>HMDS Teacher</title>
@@ -58,57 +59,54 @@
             <!-- sidebar menu -->
             <div id="sidebar-menu" class="main_menu_side hidden-print main_menu">
               <div class="menu_section">
+                <h3>General</h3>
                 <ul class="nav side-menu">
-                  <li><a><i class="fa fa-home"></i> Home <span class="fa fa-chevron-down"></span></a>
-                    <ul class="nav child_menu">
-                      <li><a href="index.html">Dashboard</a></li>
-                      <li><a href="index2.html">Dashboard2</a></li>
-                      <li><a href="index3.html">Dashboard3</a></li>
-                    </ul>
+                  <li><a href="#"><i class="fa fa-home"></i>Dashboard</a>
                   </li>
-                  <li><a><i class="fa fa-edit"></i> Forms <span class="fa fa-chevron-down"></span></a>
-                    <ul class="nav child_menu">
-                      <li><a href="form.html">General Form</a></li>
-                      <li><a href="form_advanced.html">Advanced Components</a></li>
-                      <li><a href="form_validation.html">Form Validation</a></li>
-                      <li><a href="form_wizards.html">Form Wizard</a></li>
-                      <li><a href="form_upload.html">Form Upload</a></li>
-                      <li><a href="form_buttons.html">Form Buttons</a></li>
-                    </ul>
+                </ul>
+              </div>
+              <div class="menu_section">
+                <h3>Users</h3>
+                <ul class="nav side-menu">
+                  <li><a href="#"><i class="fa fa-user"></i>Admin</a>
                   </li>
-                  <li><a><i class="fa fa-desktop"></i> UI Elements <span class="fa fa-chevron-down"></span></a>
-                    <ul class="nav child_menu">
-                      <li><a href="general_elements.html">General Elements</a></li>
-                      <li><a href="media_gallery.html">Media Gallery</a></li>
-                      <li><a href="typography.html">Typography</a></li>
-                      <li><a href="icons.html">Icons</a></li>
-                      <li><a href="glyphicons.html">Glyphicons</a></li>
-                      <li><a href="widgets.html">Widgets</a></li>
-                      <li><a href="invoice.html">Invoice</a></li>
-                      <li><a href="inbox.html">Inbox</a></li>
-                      <li><a href="calendar.html">Calendar</a></li>
-                    </ul>
+                  <li><a href="#"><i class="fa fa-users"></i>Teachers</a>
                   </li>
-                  <li><a><i class="fa fa-table"></i> Tables <span class="fa fa-chevron-down"></span></a>
-                    <ul class="nav child_menu">
-                      <li><a href="tables.html">Tables</a></li>
-                      <li><a href="tables_dynamic.html">Table Dynamic</a></li>
-                    </ul>
+                  <li><a href="#"><i class="fa fa-users"></i>Students</a>
                   </li>
-                  <li><a><i class="fa fa-bar-chart-o"></i> Data Presentation <span class="fa fa-chevron-down"></span></a>
-                    <ul class="nav child_menu">
-                      <li><a href="chartjs.html">Chart JS</a></li>
-                      <li><a href="chartjs2.html">Chart JS2</a></li>
-                      <li><a href="morisjs.html">Moris JS</a></li>
-                      <li><a href="echarts.html">ECharts</a></li>
-                      <li><a href="other_charts.html">Other Charts</a></li>
-                    </ul>
+                  <li><a href="#"><i class="fa fa-users"></i>Parents</a>
                   </li>
-                  <li><a><i class="fa fa-clone"></i>Layouts <span class="fa fa-chevron-down"></span></a>
-                    <ul class="nav child_menu">
-                      <li><a href="fixed_sidebar.html">Fixed Sidebar</a></li>
-                      <li><a href="fixed_footer.html">Fixed Footer</a></li>
-                    </ul>
+                  <li><a href="#"><i class="fa fa-user"></i>Technical Administrator</a>
+                </ul>
+              </div>
+              <div class="menu_section">
+                <h3>Class</h3>
+                <ul class="nav side-menu">
+                  <li><a href="#"><i class="fa fa-edit"></i>Assign Students To Class</a>
+                  </li>
+                  <li><a href="#"><i class="fa fa-star-o"></i>Upgrade Class</a>
+                  </li>
+                </ul>
+              </div>
+              <div class="menu_section">
+                <h3>Others</h3>
+                <ul class="nav side-menu">
+                  <li><a href="#"><i class="fa fa-edit"></i>Merits | Demerits</a>
+                  </li>
+                  <li><a href="#"><i class="fa fa-edit"></i>Punishments</a>
+                  </li>
+                  <li><a href="#"><i class="fa fa-edit"></i>Consultations</a>
+                  </li>
+                  <li><a href="#"><i class="fa fa-edit"></i>Commitments</a>
+                  </li>
+                </ul>
+              </div>
+              <div class="menu_section">
+                <h3>Report</h3>
+                <ul class="nav side-menu">
+                  <li><a href="#"><i class="fa fa-edit"></i>Audits</a>
+                  </li>
+                  <li><a href="#"><i class="fa fa-edit"></i>Reports</a>
                   </li>
                 </ul>
               </div>
