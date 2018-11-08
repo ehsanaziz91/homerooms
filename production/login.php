6b48723c0dd91c8956cfa6221bc54d921199226b
ehsanaziz91 <ehsanaziz96@gmail.com>
2018-11-08 17:41:22 +0800

    Update production/login.php

--- production/login.php
+++ production/login.php
@@ -5,40 +5,77 @@ if(isset($_POST['login']))
 {
     $userid = $_POST['userid'];
     $password = $_POST['pass']; //encrypt pass
-    $stmt = $conn->prepare("SELECT staffID, password FROM staff WHERE staffID=? AND  password=? LIMIT 1");
+    $position = "";
+    
+    $stmt = $conn->prepare("SELECT staffID, password, position FROM staff WHERE staffID=? AND password=? ");
     $stmt->bind_param('ss', $userid, $password);
     $stmt->execute();
-    $stmt->bind_result($userid, $password);
+    $stmt->bind_result($userid, $password, $position);
     $stmt->store_result();
-    
+
     if($stmt->num_rows == 1)  //To check if the row exists
     {
-        while($stmt->fetch()) //fetching the contents of the row
+        if($stmt->fetch()) //fetching the contents of the row
         {
-            session_start();
-            $_SESSION['Logged'] = 1;
-            $_SESSION['userid'] = $userid;
-            header("Location: ../production/index.php?userid=$userid");
-            //<script>$("#").load("Location: ../production/index.php?userid=$userid")</script>
-            //echo 'Success!';
-            exit();
+            if($position == '01')
+            {
+                session_start();
+                $_SESSION['Logged'] = 0;
+                $_SESSION['userid'] = $userid;
+                echo "<script type=\"text/javascript\">";
+                echo "alert('Login Success As Admin !'),location.href='admin_index.php?userid=$userid'";
+                echo "</script>";
+            }
+            else if ($position == '02')
+            {
+                session_start();
+                $_SESSION['Logged'] = 1;
+                $_SESSION['userid'] = $userid;
+                echo "<script type=\"text/javascript\">";
+                echo "alert('Login Success As Teacher !'),location.href='teacher_index.php?userid=$userid'";
+                echo "</script>";
+            }
+            else
+            {
+                echo 'Invalid User ID / Password. Please Try Again !';
+            }
+
         }
 
     }
     else 
     {
-        //echo "Invalid user id / password!";
-        //echo ("Invalid user id / password!". $stmt->error."<br>");
-        print "<script type=\"text/javascript\">";
-        print "alert('Invalid User ID / Password. Please Try Again !'),location.href='loginPage.php'";
-        print "</script>";
+        $stmts = $conn->prepare("SELECT studentID, password FROM student WHERE studentID=? AND password=? ");
+        $stmts->bind_param('ss', $userid, $password);
+        $stmts->execute();
+        $stmts->bind_result($userid, $password);
+        $stmts->store_result();
+        
+        if($stmts->num_rows == 1)  //To check if the row exists
+        {
+            while($stmts->fetch()) //fetching the contents of the row
+            {
+                session_start();
+                $_SESSION['Logged'] = 1;
+                $_SESSION['userid'] = $userid;
+                echo "<script type=\"text/javascript\">";
+                echo "alert('Login Success As Student !'),location.href='student_index.php?userid=$userid'";
+                echo "</script>";
+            }
+
+        }
+        else 
+        {
+            echo "<script type=\"text/javascript\">";
+            echo "alert('Invalid User ID / Password. Please Try Again !'),location.href='loginPage.php'";
+            echo "</script>";
+
+        }
+        $stmts->close();
+        $conn->close();
         
     }
     $stmt->close();
+    $conn->close();
 }
-else 
-{   
-
-}
-$conn->close();
 ?>
