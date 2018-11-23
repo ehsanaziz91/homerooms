f8c98f871f1418eadbbd186053bf38e79f46a2b1
ehsanaziz91 <ehsanaziz96@gmail.com>
2018-11-24 01:27:50 +0800

    Create mForm.php

---
+++ production/mForm.php
@@ -0,0 +1,72 @@
+<?php
+include('../Connections/connection.php');
+session_start();
+
+if (isset($_SESSION['userid']))
+{
+    $userid = $_SESSION['userid'];
+}else
+{
+    header ('location:../production/loginPage.php');
+};
+
+if(!empty($_POST["categoryID"])){
+    $stmt = $conn->prepare("SELECT * FROM merit WHERE categoryID = ".$_POST['categoryID']." ");
+    $stmt->execute();
+    $result = $stmt->get_result();
+
+    if($result->num_rows>0){
+        echo '<option value="">Select Merit</option>';
+        while($row = $result->fetch_assoc()){ 
+            echo '<option value="'.$row['meritID'].'">'.$row['meritName'].'</option>';
+        }
+    }else{
+        echo '<option value="">Category not available</option>';
+    }
+};
+
+if (isset($_POST['addmerit']))
+{
+    //$userid = $_GET['userid'];
+    $studid = $_POST['studid'];
+    $category = $_POST['category'];
+    $merit = $_POST['merit'];
+    $date = $_POST['date'];
+    
+    $stmt1 = $conn->prepare("SELECT categoryType FROM category WHERE categoryID = ? ");
+    $stmt1->bind_param('s',$category);
+    $stmt1->execute();
+    $result1 = $stmt1->get_result();
+    $row1 = $result1->fetch_assoc();
+    
+    $catType = $row1["categoryType"];
+    
+    
+    $stmt2 = $conn->prepare("SELECT meritName, meritPoint FROM merit WHERE categoryID = ? ");
+    $stmt2->bind_param('s',$category);
+    $stmt2->execute();
+    $result2 = $stmt2->get_result();
+    $row2 = $result2->fetch_assoc();
+    
+    $mName = $row2["meritName"];
+    $mPoint = $row2["meritPoint"];
+    
+    $stmt = $conn->prepare("INSERT INTO `record` (studentID, meritName, meritPoint, date, categoryType) VALUES(?,?,?,?,?)");
+    $stmt->bind_param('sssss', $studid, $mName, $mPoint, $date, $catType);
+    $stmt->execute();
+    if($stmt)
+    {
+        print "<script type=\"text/javascript\">";
+        print "alert('Successfully Added Merit !'),location.href='teacher_studListPageTest.php?userid=$userid'";
+        print "</script>";
+    }
+    else
+    {
+        print "<script type=\"text/javascript\">";
+        print "alert('Failure to add merit !')";
+        print "</script>";
+    }
+    $stmt->close();
+    $conn->close();
+}
+?>
