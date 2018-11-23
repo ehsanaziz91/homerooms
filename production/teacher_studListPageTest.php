bb302a6566a53600fdb82e07c8a6f9023ea89ebb
ehsanaziz91 <ehsanaziz96@gmail.com>
2018-11-24 01:29:23 +0800

    Update teacher_studListPageTest.php

--- production/teacher_studListPageTest.php
+++ production/teacher_studListPageTest.php
@@ -238,11 +238,7 @@
                                        
                                                                  <button class="btn btn-danger" name="del" onclick="document.submit();">Delete</button>
                                                                  
-                                                                 <a href="MDFormPage.php?userid='.$userid.'&studid='.$studid.'" class="btn btn-default">Merit / Demerit</a>
-                                                                 
-                                                                 <a href="defective_equip-details.php?userid='.$userid.'&studid='.$studid.'" class="btn btn-success">Merit</a>
-                                                                 
-                                                                 <a href="defective_equip-details.php?userid='.$userid.'&studid='.$studid.'" class="btn btn-warning">Demerit</a>
+                                                                 <a href="MDFormPage.php?userid='.$userid.'&studid='.$studid.'" class="btn btn-success">Merit / Demerit</a>
                                                              </form>
                                                          </td>
                                                      </tr>';
