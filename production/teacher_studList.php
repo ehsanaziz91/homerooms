876c36d8e51bafb80bd415a06a30ff2982262d3c
ehsanaziz91 <ehsanaziz96@gmail.com>
2018-11-24 01:28:44 +0800

    Update teacher_studList.php

--- production/teacher_studList.php
+++ production/teacher_studList.php
@@ -1,8 +1,19 @@
 <?php
 include('../Connections/connection.php');
 session_start();
+
+if (isset($_SESSION['userid']))
 {
-    $userid = $_POST['studid'];
+    $userid = $_SESSION['userid'];
+}else
+{
+    header ('location:../production/loginPage.php');
+};
+
+if (isset($_POST['addstudent']))
+{
+    //$userid = $_GET['userid'];
+    $studid = $_POST['studid'];
     $studname = $_POST['studname'];
     $studadd = $_POST['studadd'];
     $studno = $_POST['contc'];
@@ -13,13 +24,13 @@ session_start();
     $hrname = $_POST['hrname'];
 
     $stmt = $conn->prepare("INSERT INTO `student` (studentID, studName, studAddress, studContcNo, parentName, password, recoQuestion, recoAnswer, hrName) VALUES(?,?,?,?,?,?,?,?,?)");
-    $stmt->bind_param('sssssssss', $userid, $studname, $studadd, $studno, $parent, $pass, $questions, $answers, $hrname);
+    $stmt->bind_param('sssssssss', $studid, $studname, $studadd, $studno, $parent, $pass, $questions, $answers, $hrname);
     $stmt->execute();
 	//header('location:teacher_studListPage.php');
     if($stmt)
     {
         print "<script type=\"text/javascript\">";
-        print "alert('Successfully Registered New Student!'),location.href='teacher_studListPage.php'";
+        print "alert('Successfully Registered New Student!'),location.href='teacher_studListPage.php?userid=$userid'";
         print "</script>";
     }
     else
@@ -30,7 +41,31 @@ session_start();
     }
     $stmt->close();
     $conn->close();
-}
+};
+
+if (isset($_POST['details']))
+{
+    $studid = $_POST['studentID'];
+    
+    $stmt = $conn->prepare("SELECT studentID FROM student WHERE studentID = ?");
+    $stmt->bind_param('s', $studid);
+    $stmt->execute();
+    
+    if($stmt)
+    {
+        echo "<script type=\"text/javascript\">";
+        echo "alert('View data successfully'),location.href='teacher_studProfilePage.php?userid=$userid&studid=$studid'";
+        echo "</script>";
+    }
+    else
+    {
+        echo "<script type=\"text/javascript\">";
+        echo "alert('Could not delete data !')";
+        echo "</script>";
+    }
+    $stmt->close();
+    $conn->close();
+};
 
 //delete student
 /*if (isset($_POST['del']))
@@ -57,10 +92,45 @@ session_start();
     $conn->close();
 }*/
 
+if (isset($_POST['addmerit']))
+{
+    //$userid = $_GET['userid'];
+    //$studid = $_POST['studid'];
+    $category = $_POST['category'];
+    $merit = $_POST['merit'];
+    
+    echo $category;
+    echo $merit;
+    
+    //SELECT * FROM `merit` WHERE meritName = "Ketua Pengawas/Pusat Sumber/Koperasi/PRS"
+        
+        
+        
+
+/*    $stmt = $conn->prepare("INSERT INTO `record` (studentID, studName, studAddress, studContcNo, parentName, password, recoQuestion, recoAnswer, hrName) VALUES(?,?,?,?,?,?,?,?,?)");
+    $stmt->bind_param('sssssssss', $studid, $studname, $studadd, $studno, $parent, $pass, $questions, $answers, $hrname);
+    $stmt->execute();
+	//header('location:teacher_studListPage.php');
+    if($stmt)
+    {
+        print "<script type=\"text/javascript\">";
+        print "alert('Successfully Registered New Student!'),location.href='teacher_studListPage.php?userid=$userid'";
+        print "</script>";
+    }
+    else
+    {
+        print "<script type=\"text/javascript\">";
+        print "alert('Failure to register new Student!')";
+        print "</script>";
+    }
+    $stmt->close();
+    $conn->close();*/
+}
+
 ?>
 
-<?php
-include('../Connections/connection.php');
+<!--<?php
+/*include('../Connections/connection.php');
 session_start();
 
 if (isset($_SESSION['userid']))
@@ -93,5 +163,5 @@ if (isset($_POST['details']))
     }
     $stmt->close();
     $conn->close();
-}
-?>
+}*/
+?>-->
