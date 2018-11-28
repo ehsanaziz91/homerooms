317f923abc3f0acbaa7960d35be6fe722b384d66
ehsanaziz91 <ehsanaziz96@gmail.com>
2018-11-28 22:31:18 +0800

    Update registration.php

--- production/registration.php
+++ production/registration.php
@@ -1,34 +1,35 @@
 <?php
 include('../Connections/connection.php');
-session_start();
 
 if(isset($_POST['submit']))
 {
-    $id = $_POST['staffid'];
+    $userid = $_POST['staffid'];
     $name = $_POST['fullName'];
     $positions = $_POST['position'];
     $phone = $_POST['phoneNo'];
     $emails = $_POST['email'];
-    $password = $_POST['pass']; //encrypt pass
+    $password = md5($_POST['pass']); //encrypt pass
     $questions = $_POST['question'];
     $answers = $_POST['answer'];
 
     $stmt = $conn->prepare("INSERT INTO `staff` (staffID, staffName, position, phoneNo, email, password, recoQuestion, recoAnswer) VALUES(?,?,?,?,?,?,?,?)");
-    $stmt->bind_param("ssssssss", $id, $name, $positions, $phone, $emails, $password, $questions, $answers);
+    $stmt->bind_param("ssssssss", $userid, $name, $positions, $phone, $emails, $password, $questions, $answers);
     $stmt->execute();
 
     if ($stmt)
     {
         print "<script type=\"text/javascript\">";
-        print "alert('Successfully Registered'),location.href='loginPage.php'";
+        print "alert('Successfully Registered !'),location.href='loginPage.php'";
         print "</script>";
     }
     else
     {
         print "<script type=\"text/javascript\">";
-        print "alert('Problem Occured')";
+        print "alert('Problem Occured !')";
         print "</script>";
     }
-}
+    $stmt->close();
+    $conn->close();
+}    
 
 ?>
