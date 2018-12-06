9e8070e6dabdc12babde48b7f501110134f77e3f
ehsanaziz91 <ehsanaziz96@gmail.com>
2018-12-06 09:55:03 +0800

    Update demerit_stage.php

--- production/demerit_stage.php
+++ production/demerit_stage.php
@@ -1,16 +1,15 @@
 <?php
-include('../Connections/connection.php');
-session_start();
+    include('../Connections/connection.php');
+    session_start();
 
-if (isset($_SESSION['userid']))
-{
-    $userid = $_SESSION['userid'];
-}else
-{
-    header ('location:../production/loginPage.php');
-}
+    if (isset($_SESSION['userid']))
+    {
+       $userid = $_SESSION['userid'];
+    }else
+    {
+       header ('location:../production/loginPage.php');
+    }
 ?>
-
 <!DOCTYPE html>
 <html lang="en">
   <head>
@@ -19,10 +18,8 @@ if (isset($_SESSION['userid']))
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <!--<meta http-equiv="REFRESH" content="10; url=http://localhost/homerooms/production/admin_index.php"> <!--loading page in every 10 second-->
-	<!--<link rel="icon" href="images/favicon.ico" type="image/ico" />-->
 
-    <title>HMDS Admin</title>
+    <title>HMDS Demerit Stage</title>
 
     <!-- Bootstrap -->
     <link href="../vendors/bootstrap/dist/css/bootstrap.min.css" rel="stylesheet">
@@ -32,13 +29,12 @@ if (isset($_SESSION['userid']))
     <link href="../vendors/nprogress/nprogress.css" rel="stylesheet">
     <!-- iCheck -->
     <link href="../vendors/iCheck/skins/flat/green.css" rel="stylesheet">
-	
-    <!-- bootstrap-progressbar -->
-    <link href="../vendors/bootstrap-progressbar/css/bootstrap-progressbar-3.3.4.min.css" rel="stylesheet">
-    <!-- JQVMap -->
-    <link href="../vendors/jqvmap/dist/jqvmap.min.css" rel="stylesheet"/>
-    <!-- bootstrap-daterangepicker -->
-    <link href="../vendors/bootstrap-daterangepicker/daterangepicker.css" rel="stylesheet">
+    <!-- Datatables -->
+    <link href="../vendors/datatables.net-bs/css/dataTables.bootstrap.min.css" rel="stylesheet">
+    <link href="../vendors/datatables.net-buttons-bs/css/buttons.bootstrap.min.css" rel="stylesheet">
+    <link href="../vendors/datatables.net-fixedheader-bs/css/fixedHeader.bootstrap.min.css" rel="stylesheet">
+    <link href="../vendors/datatables.net-responsive-bs/css/responsive.bootstrap.min.css" rel="stylesheet">
+    <link href="../vendors/datatables.net-scroller-bs/css/scroller.bootstrap.min.css" rel="stylesheet">
 
     <!-- Custom Theme Style -->
     <link href="../build/css/custom.min.css" rel="stylesheet">
@@ -54,30 +50,17 @@ if (isset($_SESSION['userid']))
             </div>
 
             <div class="clearfix"></div>
-  <?php
-                                        include('../Connections/connection.php');
 
-                                        if (isset($_GET['userid']))
-                                        {
-                                            $stmt = $conn->prepare("SELECT * FROM staff WHERE staffID = ?");
-                                            $stmt->bind_param('s', $userid);
-                                            $stmt->execute();
-                                            $result = $stmt->get_result();
-                                            $row = $result->fetch_assoc();
-
-                                            $staffname = $row['staffName'];
-                                        }
-              ?>
             <!-- menu profile quick info -->
-            <div class="profile clearfix">
+<!--            <div class="profile clearfix">
               <div class="profile_pic">
-                <img src="images/img.JPG" alt="..." class="img-circle profile_img">
+                <img src="images/img.jpg" alt="..." class="img-circle profile_img">
               </div>
               <div class="profile_info">
-                <span>Admin</span>
-                <h2><?php echo $staffname;?></h2>
+                <span>Welcome,</span>
+                <h2>John Doe</h2>
               </div>
-            </div>
+            </div>-->
             <!-- /menu profile quick info -->
 
             <br />
@@ -85,25 +68,26 @@ if (isset($_SESSION['userid']))
             <!-- sidebar menu -->
             <div id="sidebar-menu" class="main_menu_side hidden-print main_menu">
               <div class="menu_section">
-                <h3>General</h3>
+                <h3>Actions</h3>
                 <ul class="nav side-menu">
-                  <li><a href="admin_index.php?userid=<?php echo $userid; ?>"><i class="fa fa-home"></i>Dashboard</a>
-                  </li>
+                    <li><a href="MDFormPageAdmin.php?userid=<?php echo $userid; ?>"><i class="fa fa-home"></i>Merit & Demerit</a></li>
+                    <li><a href="demerit_stage.php?userid=<?php echo $userid; ?>"><i class="fa fa-home"></i>Demerit Stage</a></li>
+                    <li><a href="merit_schedule.php?userid=<?php echo $userid; ?>"><i class="fa fa-home"></i>Merit & Demerit Schedule</a></li>
                 </ul>
               </div>
-              <div class="menu_section">
+<!--              <div class="menu_section">
                 <h3>Users</h3>
                 <ul class="nav side-menu">
-                   <li><a href="#"><i class="fa fa-users"></i>Admin<span class="fa fa-chevron-down"></span></a>
+                  <li><a href="#"><i class="fa fa-users"></i>Admin<span class="fa fa-chevron-down"></span></a>
                        <ul class="nav child_menu">
-                       <li><a href="MDFormPageAdmin.php?userid=<?php echo $userid; ?>">Merit & Demerit</a></li>
-                      <li><a href="demerit_stage.php?userid=<?php echo $userid; ?>">Demerit Stage</a></li>
-                      <li><a href="merit_schedule.php?userid=<?php echo $userid; ?>">Merit & Demerit Schedule</a></li>
-                      <li><a href="#">Assign Students</a></li> <!--kalau sempat, buat form utk assign student-->
+                       <li><a href="MDFormPageAdmin.php?userid=<?php //echo $userid; ?>">Merit & Demerit</a></li>
+                      <li><a href="demerit_stage.php?userid=<?php //echo $userid; ?>">Demerit Stage</a></li>
+                      <li><a href="merit_schedule.php?userid=<?php //echo $userid; ?>">Merit & Demerit Schedule</a></li>
+                      <li><a href="#">Assign Students</a></li> kalau sempat, buat form utk assign student
                       </ul>
                   </li>
                 </ul>
-              </div>
+              </div>-->
               <div class="menu_section">
                 <h3>Report</h3>
                 <ul class="nav side-menu">
@@ -117,7 +101,8 @@ if (isset($_SESSION['userid']))
 
             <!-- /menu footer buttons -->
             <div class="sidebar-footer hidden-small">
-              <a data-toggle="tooltip" data-placement="top" title="Logout" href="loginPage.php">
+              
+              <a data-toggle="tooltip" data-placement="top" title="Logout" href="logout.php">
                 <span class="glyphicon glyphicon-off" aria-hidden="true"></span>
               </a>
             </div>
@@ -136,8 +121,28 @@ if (isset($_SESSION['userid']))
               <ul class="nav navbar-nav navbar-right">
                 <li class="">
                   <a href="javascript:;" class="user-profile dropdown-toggle" data-toggle="dropdown" aria-expanded="false">
-                    <img src="images/img.JPG" alt=""><?php echo $staffname;?>
-                    <span class=" fa fa-angle-down"></span>
+                      <?php
+                        include('../Connections/connection.php');
+
+                        if (isset($_GET['userid']))
+                        {
+                            $stmt = $conn->prepare("SELECT * FROM staff WHERE staffID = ?");
+                            $stmt->bind_param('s', $userid);
+                            $stmt->execute();
+                            $result = $stmt->get_result();
+                            $row = $result->fetch_assoc();
+                            if($row['img'] == NULL)
+                            {
+                                echo '<div><img src="images/user.png">'.$row['staffName'].'  <span class=" fa fa-angle-down"></span></div>';
+                            }
+                            else
+                            {
+                                echo '<div><img src="data:images/JPG;base64,'.base64_encode( $row['img'] ).'"/>'.$row['staffName'].'  <span class=" fa fa-angle-down"></span></div>';
+                            }
+                        }
+                    ?>
+                    <!--<img src="images/user.png" alt=""><?php //echo $staffname;?>-->
+                    <!--<span class=" fa fa-angle-down"></span>-->
                   </a>
                   <ul class="dropdown-menu dropdown-usermenu pull-right">
                     <li><a href="admin_profilePage.php?userid=<?php echo $userid; ?>"> Profile</a></li>
@@ -146,103 +151,86 @@ if (isset($_SESSION['userid']))
                   </ul>
                 </li>
 
-                <li role="presentation" class="dropdown">
+<!--                <li role="presentation" class="dropdown">
                   <a href="javascript:;" class="dropdown-toggle info-number" data-toggle="dropdown" aria-expanded="false">
                     <i class="fa fa-envelope-o"></i>
                     <span class="badge bg-green">6</span>
                   </a>
-                </li>
+                </li>-->
               </ul>
             </nav>
           </div>
         </div>
         <!-- /top navigation -->
 
-      
         <!-- page content -->
         <div class="right_col" role="main">
           <div class="">
             <div class="page-title">
               <div class="title_left">
-                <h3></h3>
-              </div>
-
-              <div class="title_right">
-                <div class="col-md-5 col-sm-5 col-xs-12 form-group pull-right top_search">
-                  <div class="input-group">
-                  </div>
-                </div>
+                <h3>Demerit Stage</h3>
               </div>
             </div>
 
             <div class="clearfix"></div>
-
+              
             <div class="row">
               <div class="col-md-12 col-sm-12 col-xs-12">
-                <div class="x_panel" style="height:500px;">
+                <div class="x_panel">
                   <div class="x_title">
-                    <h2>Demerit Stage</h2>
-                    <ul class="nav navbar-right panel_toolbox">
-                      <li><a class="collapse-link"><i class="fa fa-chevron-up"></i></a>
-                      </li>
-                      <li class="dropdown">
-                        <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false"><i class="fa fa-wrench"></i></a>
-                      </li>
-                      <li><a class="close-link"><i class="fa fa-close"></i></a>
-                      </li>
-                    </ul>
+                    <h2>List of Demerit Stage</h2>
                     <div class="clearfix"></div>
                   </div>
-
                   <div class="x_content">
-                    <div class="row">
+                    <table id="datatable" class="table table-striped table-bordered">
+                      <thead>
+                        <tr>
+                          <th>Stage ID</th>
+                          <th>Current Point</th>
+                          <th>Descriptions</th>
+                        </tr>
+                      </thead>
 
-                      <div class="col-md-12">
-                           <div class="x_content">
-                              <table id="datatable-buttons" class="table table-striped table-bordered">
-                                 <thead>
-                                    <tr>
-                                       <th>Stage ID</th>
-                                       <th>Current Point</th>
-                                       <th>Description</th>
-                                    </tr>
-                                 </thead>
-                                 <tbody>
-                                    <?php
-                                       include('../Connections/connection.php');
-                                       
-                                         if($stmt = $conn->prepare("SELECT stageID, currentPoint, description FROM stage")) 
-                                         {
-                                             $stmt -> execute();
-                                             $stmt -> bind_result($stageID, $currentPoint, $description);
-                                             while($stmt->fetch()) 
-                                             {
-                                                 echo '<tr>
-                                                         <td>' . $stageID . '</td>
-                                                         <td>' . $currentPoint . '</td>
-                                                         <td>' . $description . '</td>
-                                                     </tr>';
-                                             }
-                                         }
-                                       $stmt->close();
-                                       $conn->close();
-                                       ?>
-                                 </tbody>
-                              </table>
-                           </div>
-                       
-                      </div>
-                    </div>
+                      <tbody>
+                        <?php
+                           include('../Connections/connection.php');
+                          
+                             if($stmt = $conn->prepare("SELECT stageID, currentPoint, description FROM stage")) 
+                             {
+                                 $stmt -> execute();
+                                 $stmt -> bind_result($stage, $cPoint, $desc);
+                                 while($stmt->fetch()) 
+                                 {
+                                     echo '<tr>
+                                             <td>' . $stage . '</td>
+                                             <td>' . $cPoint . '</td>
+                                             <td>' . $desc . '</td>
+                                         </tr>';
+                                 }
+                                   $stmt->close();
+                                   $conn->close();
+                             }
+                           ?>
+                      </tbody>
+                    </table>
                   </div>
                 </div>
-                  
               </div>
+
             </div>
+              
           </div>
         </div>
         <!-- /page content -->
 
-
+        <!-- footer content -->
+        <footer>
+          <div class="pull-right">
+            HMD System@2018/19 - WorkshopII | BITS | FTMK | UTeM
+          </div>
+          <div class="clearfix"></div>
+        </footer>
+        <!-- /footer content -->
       </div>
     </div>
 
@@ -254,38 +242,27 @@ if (isset($_SESSION['userid']))
     <script src="../vendors/fastclick/lib/fastclick.js"></script>
     <!-- NProgress -->
     <script src="../vendors/nprogress/nprogress.js"></script>
-    <!-- Chart.js -->
-    <script src="../vendors/Chart.js/dist/Chart.min.js"></script>
-    <!-- gauge.js -->
-    <script src="../vendors/gauge.js/dist/gauge.min.js"></script>
-    <!-- bootstrap-progressbar -->
-    <script src="../vendors/bootstrap-progressbar/bootstrap-progressbar.min.js"></script>
     <!-- iCheck -->
     <script src="../vendors/iCheck/icheck.min.js"></script>
-    <!-- Skycons -->
-    <script src="../vendors/skycons/skycons.js"></script>
-    <!-- Flot -->
-    <script src="../vendors/Flot/jquery.flot.js"></script>
-    <script src="../vendors/Flot/jquery.flot.pie.js"></script>
-    <script src="../vendors/Flot/jquery.flot.time.js"></script>
-    <script src="../vendors/Flot/jquery.flot.stack.js"></script>
-    <script src="../vendors/Flot/jquery.flot.resize.js"></script>
-    <!-- Flot plugins -->
-    <script src="../vendors/flot.orderbars/js/jquery.flot.orderBars.js"></script>
-    <script src="../vendors/flot-spline/js/jquery.flot.spline.min.js"></script>
-    <script src="../vendors/flot.curvedlines/curvedLines.js"></script>
-    <!-- DateJS -->
-    <script src="../vendors/DateJS/build/date.js"></script>
-    <!-- JQVMap -->
-    <script src="../vendors/jqvmap/dist/jquery.vmap.js"></script>
-    <script src="../vendors/jqvmap/dist/maps/jquery.vmap.world.js"></script>
-    <script src="../vendors/jqvmap/examples/js/jquery.vmap.sampledata.js"></script>
-    <!-- bootstrap-daterangepicker -->
-    <script src="../vendors/moment/min/moment.min.js"></script>
-    <script src="../vendors/bootstrap-daterangepicker/daterangepicker.js"></script>
+    <!-- Datatables -->
+    <script src="../vendors/datatables.net/js/jquery.dataTables.min.js"></script>
+    <script src="../vendors/datatables.net-bs/js/dataTables.bootstrap.min.js"></script>
+    <script src="../vendors/datatables.net-buttons/js/dataTables.buttons.min.js"></script>
+    <script src="../vendors/datatables.net-buttons-bs/js/buttons.bootstrap.min.js"></script>
+    <script src="../vendors/datatables.net-buttons/js/buttons.flash.min.js"></script>
+    <script src="../vendors/datatables.net-buttons/js/buttons.html5.min.js"></script>
+    <script src="../vendors/datatables.net-buttons/js/buttons.print.min.js"></script>
+    <script src="../vendors/datatables.net-fixedheader/js/dataTables.fixedHeader.min.js"></script>
+    <script src="../vendors/datatables.net-keytable/js/dataTables.keyTable.min.js"></script>
+    <script src="../vendors/datatables.net-responsive/js/dataTables.responsive.min.js"></script>
+    <script src="../vendors/datatables.net-responsive-bs/js/responsive.bootstrap.js"></script>
+    <script src="../vendors/datatables.net-scroller/js/dataTables.scroller.min.js"></script>
+    <script src="../vendors/jszip/dist/jszip.min.js"></script>
+    <script src="../vendors/pdfmake/build/pdfmake.min.js"></script>
+    <script src="../vendors/pdfmake/build/vfs_fonts.js"></script>
 
     <!-- Custom Theme Scripts -->
     <script src="../build/js/custom.min.js"></script>
-	
+
   </body>
 </html>
