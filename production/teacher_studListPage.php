e4100239f3deec8fa98d0f24b6abcd094f7bda27
najihah <ajirasul@gmail.com>
2018-12-07 07:44:17 +0800

    Update teacher_studListPage.php

--- production/teacher_studListPage.php
+++ production/teacher_studListPage.php
@@ -124,36 +124,39 @@
                   <!-- /menu footer buttons -->
                </div>
             </div>
-            <!-- top navigation -->
-            <div class="top_nav">
-               <div class="nav_menu">
-                  <nav>
-                     <div class="nav toggle">
-                        <a id="menu_toggle"><i class="fa fa-bars"></i></a>
-                     </div>
-                     <ul class="nav navbar-nav navbar-right">
-                        <li class="">
-                           <a href="javascript:;" class="user-profile dropdown-toggle" data-toggle="dropdown" aria-expanded="false">
-                           <img src="images/img.jpg" alt=""><?php echo $staffname;?>
-                           <span class=" fa fa-angle-down"></span>
-                           </a>
-                           <ul class="dropdown-menu dropdown-usermenu pull-right">
-                    <li><a href="admin_profilePage.php?userid=<?php echo $userid; ?>"> Profile</a></li>
+         <!-- top navigation -->
+        <div class="top_nav">
+          <div class="nav_menu">
+            <nav>
+              <div class="nav toggle">
+                <a id="menu_toggle"><i class="fa fa-bars"></i></a>
+              </div>
+
+              <ul class="nav navbar-nav navbar-right">
+                <li class="">
+                  <a href="javascript:;" class="user-profile dropdown-toggle" data-toggle="dropdown" aria-expanded="false">
+                    <img src="images/user.png" alt=""><?php echo $staffname;?>
+                    <span class=" fa fa-angle-down"></span>
+                  </a>
+                  <ul class="dropdown-menu dropdown-usermenu pull-right">
+                    <li><a href="teacher_profilePage.php?userid=<?php echo $userid; ?>"> Profile</a></li>
+                    <!--<li><a href="testing.php?userid=<?php //echo $userid; ?>"> Testing</a></li>-->
                     <li><a href="recoveryPage.php?userid=<?php echo $userid; ?>"> Change Password</a></li>
                     <li><a href="logout.php"><i class="fa fa-sign-out pull-right"></i> Log Out</a></li>
                   </ul>
-                        </li>
-                        <li role="presentation" class="dropdown">
-                           <a href="javascript:;" class="dropdown-toggle info-number" data-toggle="dropdown" aria-expanded="false">
-                           <!--<i class="fa fa-envelope-o"></i>-->
-                           <!--<span class="badge bg-green">6</span>-->
-                           </a>
-                        </li>
-                     </ul>
-                  </nav>
-               </div>
-            </div>
-            <!-- /top navigation -->
+                </li>
+
+                <li role="presentation" class="dropdown">
+                  <a href="javascript:;" class="dropdown-toggle info-number" data-toggle="dropdown" aria-expanded="false">
+                  <!--  <i class="fa fa-envelope-o"></i>
+                    <span class="badge bg-green">6</span>-->
+                  </a>
+                </li>
+              </ul>
+            </nav>
+          </div>
+        </div>
+        <!-- /top navigation -->
             <!-- page content -->
             <div class="right_col" role="main">
                <div class="">
