3e4799cb93d6bbbc930fa05c818abadc2e1c3702
najihah <ajirasul@gmail.com>
2018-12-11 05:38:26 +0800

    Update teacherDemerit_stage.php

--- production/teacherDemerit_stage.php
+++ production/teacherDemerit_stage.php
@@ -50,7 +50,7 @@ if (isset($_SESSION['userid']))
         <div class="col-md-3 left_col">
           <div class="left_col scroll-view">
             <div class="navbar nav_title" style="border: 0;">
-              <a href="index.html" class="site_title"><i class="fa fa-university"></i> <span>HMD System</span></a>
+              <a href="#" class="site_title"><i class="fa fa-university"></i> <span>HMD System</span></a>
             </div>
 
             <div class="clearfix"></div>
