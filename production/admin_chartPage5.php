4ca1fafb4a3e5e195511ed907f56b1551ef3e660
najihah <ajirasul@gmail.com>
2018-12-12 05:16:18 +0800

    Update admin_chartPage5.php

--- production/admin_chartPage5.php
+++ production/admin_chartPage5.php
@@ -109,9 +109,7 @@
             <!-- menu footer buttons -->
             <div class="sidebar-footer hidden-small">
 
-              <a data-toggle="tooltip" data-placement="top" title="Logout" href="logout.php">
-                <span class="glyphicon glyphicon-off" aria-hidden="true"></span>
-              </a>
+              
             </div>
             <!-- menu footer buttons -->
           </div>
